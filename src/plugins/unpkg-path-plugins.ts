8f26abf5527fcf2a825869c6d89e75cb1162354a
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2021-06-07 13:28:38 -0400

    refactored onResolve handlers with complex conditionals into three separate handlers that use reg expression filters instead

--- src/plugins/unpkg-path-plugins.ts
+++ src/plugins/unpkg-path-plugins.ts
@@ -12,22 +12,25 @@ export const unpkgPathPlugin = (inputCode: string) => {
     setup(build: esbuild.PluginBuild) {
       // event listener "onResolve", which figures out the path to the index.js file
       // we're hijacking the path
+
+      // Handle root entry file of 'index.js'
+      build.onResolve({ filter: /(^index\.js$)/ }, () => {
+        return { path: 'index.js', namespace: 'a' };
+      });
+
+      // Handle relative paths in a module
+      build.onResolve({ filter: /^\.+\// }, (args: any) => {
+        return {
+          namespace: 'a',
+          path: new URL(
+            args.path,
+            'https://unpkg.com' + args.resolveDir + '/'
+          ).href,
+        };
+      });
+
+      // Handle main file of a module 
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResolve', args);
-        if (args.path === 'index.js') {
-          return { path: args.path, namespace: 'a' }; // path to index.js is given property of args
-        }
-
-        if (args.path.includes('./') || args.path.includes('../')) {
-          return {
-            namespace: 'a',
-            path: new URL(
-              args.path,
-              'https://unpkg.com' + args.resolveDir + '/'
-            ).href,
-          };
-        }
-
         return {
           namespace: 'a',
           path: `https://unpkg.com/${args.path}`,
