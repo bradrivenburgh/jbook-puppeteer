0daeaa9b1cfda5222c3f508f2c2552a0124684a5
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2021-06-08 10:07:10 -0400

    fetch-plugin now is able to import css files from unpkg

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -11,20 +11,16 @@ export const fetchPlugin = (inputCode: string) => {
     name: 'fetch-plugin',
     setup(build: esbuild.PluginBuild) {
       // event listener "onLoad", when esbuild tries to load a file
-      // again, we're hijacking the path again and returning an object instead of a file
+      // again, we're hijacking the path again and returning an object
+      // instead of a file
+
+      build.onLoad({ filter: /(^index\.js$)/ }, () => {
+        return { loader: 'jsx', contents: inputCode };
+      });
+
+      // Handles checking the cache and returning from it.  onLoad handlers
+      // do not need to have a return value
       build.onLoad({ filter: /.*/ }, async (args: any) => {
-        if (args.path === 'index.js') {
-          // this object is returned instead of index.js
-          return {
-            loader: 'jsx',
-            contents: inputCode,
-          };
-        }
-
-        // Implemented with axios library.  I like it better because
-        // it's much more compact and .get() doesn't require you to know
-        // if the format of the data being returned (text vs. JSON)
-
         // Checked to see if we have already fetched this file
         // and if it is in the cache
         const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
@@ -35,7 +31,37 @@ export const fetchPlugin = (inputCode: string) => {
         if (cachedResult) {
           return cachedResult;
         }
+      });
 
+      build.onLoad({ filter: /.css$/ }, async (args: any) => {
+        const { data, request } = await axios.get(args.path);
+
+        // esbuild wants to write css to an output file, which we do not want.
+        // Instead, we will write css to style element in the index.html's head
+        // element.  The css content needs newlines, double quotes, and single
+        // quotes escaped in order to be processed correctly.
+        const escaped = data
+          .replace(/\n/g, '')
+          .replace(/"/g, '\\"')
+          .replace(/'/g, "\\'");
+        const contents = `
+            const style = document.createElement('style');
+            style.innerText = '${escaped}';
+            document.head.appendChild(style);
+          `;
+
+        const result: esbuild.OnLoadResult = {
+          loader: 'jsx',
+          contents,
+          resolveDir: new URL('./', request.responseURL).pathname,
+        };
+
+        // store response in cache
+        await fileCache.setItem(args.path, result);
+        return result;
+      });
+
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
         const { data, request } = await axios.get(args.path);
 
         const result: esbuild.OnLoadResult = {
@@ -46,29 +72,7 @@ export const fetchPlugin = (inputCode: string) => {
 
         // store response in cache
         await fileCache.setItem(args.path, result);
-
         return result;
-
-        // Implemented with fetch API
-
-        // let data;
-        // await fetch(args.path)
-        //   .then((response) => {
-        //     if (!response.ok) {
-        //       return;
-        //     } else {
-        //       return response.text();
-        //     }
-        //   })
-        //   .then((responseText) => {
-        //     data = responseText;
-        //   })
-        //   .catch((e) => console.log(e.message));
-
-        // return {
-        //   loader: 'jsx',
-        //   contents: data,
-        // };
       });
     },
   };
