384418882f0ee807991beae4e499e8d62f155512
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2021-06-15 10:21:39 -0400

    refactored  iframe into Preview component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import 'bulmaswatch/superhero/bulmaswatch.min.css'
+import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import * as esbuild from 'esbuild-wasm';
 import { useState, useEffect, useRef } from 'react';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugins';
 import { fetchPlugin } from './plugins/fetch-plugin';
 import CodeEditor from './components/code-editor';
+import Preview from './components/preview';
 
 const App = () => {
   const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
   const serviceRef = useRef<any>(null);
-  const iframe = useRef<any>(null);
 
   const startService = async () => {
     serviceRef.current = await esbuild.startService({
@@ -26,8 +27,6 @@ const App = () => {
       return;
     }
 
-    iframe.current.srcdoc = html;
-
     const result = await serviceRef.current.build({
       entryPoints: ['index.js'], // first file to be bundled in application
       bundle: true,
@@ -39,29 +38,8 @@ const App = () => {
       },
     });
 
-    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
+    setCode(result.outputFiles[0].text);
   };
-
-  const html = `
-    <html>
-      <head>
-      </head>
-      <body>
-        <div id="root"></div>
-        <script>
-          window.addEventListener('message', (event) => {
-            try {
-              eval(event.data);
-            } catch (error) {
-              const root = document.querySelector('#root');
-              root.innerHTML = '<div style="color: red;"> <h4>Runtime Error</h4>' + error + '</div>';
-              console.error(error)
-            }
-          }, false)
-        </script>
-      </body>
-    </html>
-  `;
 
   return (
     <div>
@@ -69,19 +47,12 @@ const App = () => {
         onChange={(value) => setInput(value)}
         initialValue="document.querySelector('#root').innerHTML = 'Hello World';"
       />
-      {/* <textarea onChange={(e) => setInput(e.target.value)} value={input} /> */}
       <div>
         <button id='submit' onClick={onClick}>
           Submit
         </button>
       </div>
-      <iframe
-        ref={iframe}
-        srcDoc={html}
-        sandbox='allow-scripts'
-        title='codeOutput'
-        name='codeOutput'
-      />
+      <Preview code={code} />
     </div>
   );
 };
