63dd11faee617f90ebe49dee89aae9ff2c70dc29
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2021-06-15 10:49:35 -0400

    refactored bundler logic out of App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,16 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-import * as esbuild from 'esbuild-wasm';
-import { useState, useEffect, useRef } from 'react';
-import { unpkgPathPlugin } from './plugins/unpkg-path-plugins';
-import { fetchPlugin } from './plugins/fetch-plugin';
+import { useState } from 'react';
 import CodeEditor from './components/code-editor';
 import Preview from './components/preview';
+import bundle from './bundler';
 
 const App = () => {
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
-  const serviceRef = useRef<any>(null);
-
-  const startService = async () => {
-    serviceRef.current = await esbuild.startService({
-      worker: true,
-      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
-    });
-  };
-
-  useEffect(() => {
-    startService();
-  }, []);
 
   const onClick = async () => {
-    if (!serviceRef.current) {
-      return;
-    }
-
-    const result = await serviceRef.current.build({
-      entryPoints: ['index.js'], // first file to be bundled in application
-      bundle: true,
-      write: false,
-      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-      define: {
-        'process.env.NODE_ENV': '"production"', // define variable as "production" str
-        global: 'window', // replace instances of 'global' var with 'window' var (browser global)
-      },
-    });
-
-    setCode(result.outputFiles[0].text);
+    const output = await bundle(input);
+    setCode(output);
   };
 
   return (
--- src/bundler/index.ts
+++ src/bundler/index.ts
@@ -1,4 +1,28 @@
+import * as esbuild from 'esbuild-wasm';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugins';
+import { fetchPlugin } from './plugins/fetch-plugin';
 
-export default (rawCode: string) => {
-  
-}
+let service: esbuild.Service;
+const bundle = async (rawCode: string) => {
+  if (!service) {
+    service = await esbuild.startService({
+      worker: true,
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
+    });
+  }
+
+  const result = await service.build({
+    entryPoints: ['index.js'], // first file to be bundled in application
+    bundle: true,
+    write: false,
+    plugins: [unpkgPathPlugin(), fetchPlugin(rawCode)],
+    define: {
+      'process.env.NODE_ENV': '"production"', // define variable as "production" str
+      global: 'window', // replace instances of 'global' var with 'window' var (browser global)
+    },
+  });
+
+  return result.outputFiles[0].text;
+};
+
+export default bundle;
