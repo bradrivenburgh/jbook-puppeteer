faf11fb6f2e645b37321f604587d76c756f6bd89
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2021-06-18 10:06:27 -0400

    fleshed out cellReducer cases

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -1,3 +1,5 @@
+import { randomInt } from 'crypto';
+import { stat } from 'fs';
 import produce from 'immer';
 import { ActionType } from '../action-types';
 import { Action } from '../actions';
@@ -22,13 +24,38 @@ const initialState: CellsState = {
 const reducer = produce((state: CellsState = initialState, action: Action) => {
   switch (action.type) {
     case ActionType.MOVE_CELL:
-      return state;
+      const { direction } = action.payload;
+      const index = state.order.findIndex((id) => id === action.payload.id);
+      const targetIndex = direction === 'up' ? index - 1 : index + 1;
+
+      if (targetIndex < 0 || targetIndex > state.order.length - 1) {
+        return;
+      }
+
+      state.order[index] = state.order[targetIndex];
+      state.order[targetIndex] = action.payload.id;
+      return;
     case ActionType.DELETE_CELL:
       delete state.data[action.payload];
       state.order = state.order.filter((id) => id !== action.payload);
       return;
     case ActionType.INSERT_CELL_BEFORE:
-      return state;
+      const cell: Cell = {
+        content: '',
+        type: action.payload.type,
+        id: randomId(),
+      };
+
+      state.data[cell.id] = cell;
+      const foundIndex = state.order.findIndex(
+        (id) => id === action.payload.id
+      );
+      if (foundIndex < 0) {
+        state.order.push(cell.id);
+      } else {
+        state.order.splice(foundIndex, 0, cell.id);
+      }
+      return;
     case ActionType.UPDATE_CELL:
       const { id, content } = action.payload;
       state.data[id].content = content;
@@ -37,5 +64,9 @@ const reducer = produce((state: CellsState = initialState, action: Action) => {
       return state;
   }
 });
+
+const randomId = () => {
+  return Math.random().toString(36).substring(2, 5); // nice randomization function
+};
 
 export default reducer;
