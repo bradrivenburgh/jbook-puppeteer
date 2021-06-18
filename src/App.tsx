4342a6945a68931b4d3b2ba7e4e156d507e21964
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2021-06-18 16:55:12 -0400

    added code-list and code-list-item components; added convenience hook useActions

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-import TextEditor from './components/text-editor';
+import CellList from './components/cell-list';
 import { Provider } from 'react-redux';
 import { store } from './state';
 
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <Provider store={store}>
       <div>
-        <TextEditor />
+        <CellList />
       </div>
     </Provider>
   );
